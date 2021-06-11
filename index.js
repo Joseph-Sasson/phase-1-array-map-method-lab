907967aa77b458b414448b11c25c3c4f531460ce
Joseph <josephsasson1@gmail.com>
2021-06-10 22:42:03 -0400

    commit tree 3a16e9e20cba37f70c4fcc5f52350058c9e873cc

--- index.js
+++ index.js
@@ -12,5 +12,23 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
-}
+  return tutorials.map(sentance => {
+    let tutorialArray = sentance.split(" ")
+    return tutorialArray.map(word => word[0].toUpperCase() + word.substring(1)).join(" ")
+  })}
+
+
+// FROM ZOOM WITH ANDY
+// const titleCased = () => {
+//   //tutorials = an array of sentences
+//   let mappedTutorials = tutorials.map(sentence => {
+//     // .split splits the sentence into an array of words which we name tutorialArray
+//     let tutorialArray = sentence.split(" ")
+//     // the .map below capitalizes the first letter of each word in the word array and joins them back into a string
+//     let capSentence = tutorialArray.map(word => word[0].toUpperCase() + word.substring(1)).join(" ")
+//     return capSentence
+//   })
+//   return mappedTutorials
+//   // cap.map(first => first[0].toUpperCase() + first.substring(1)).join(" ")
+//   //   return tutorials
+// }
